1c5c4950fe035efb53ac88c3b6a1154a52d1cfc4
PMGames <pratyush.mahadevaiah@gmail.com>
2023-10-21 11:53:39 -0700

    added polish

--- Aurah Hackathon Final/Assets/QuoteLogic.cs
+++ Aurah Hackathon Final/Assets/QuoteLogic.cs
@@ -5,21 +5,27 @@ using TMPro;
 
 public class QuoteLogic : MonoBehaviour
 {
+    //create a list of strings that we can modify from the editor itself
     [SerializeField] List<string> quotes = new List<string>();
 
+    //grab the quote text holder and store it in a referenced variable
     [SerializeField] TMP_Text quoteText;
 
+    //find the quote number the user is currently on
     int count = 0;
 
     public void NewQuote()
     {
+        //this checks if the user is on the last quote. if so, set the count to 0
         if (count == quotes.Count-1)
             count = 0;
         else
             count++;
 
+        //create a new int to store the value
         int quote = count;
 
+        //set the text to the referenced quote
         quoteText.text = quotes[quote].ToString();
     }
 }
--- Aurah Hackathon Final/Assets/Stopwatch.cs
+++ Aurah Hackathon Final/Assets/Stopwatch.cs
@@ -5,15 +5,20 @@ using TMPro;
 
 public class Stopwatch : MonoBehaviour
 {
+    //the start bool tells the code when to start the stopwatch
     public bool start;
+    //this TMP_Text references the visual text which the user will see
     [SerializeField] TMP_Text watch;
+    //the value is the time elapsed
     float value = 0;
 
+    //once this function is called with the button, it will set the start value based on the parameter passed
     public void SetBool(bool val)
     {
         start = val;
     }
 
+    //reset will allow the player to restart his focus session
     public void Reset()
     {
         start = false;
@@ -25,8 +30,11 @@ public class Stopwatch : MonoBehaviour
         if (start == false)
             return;
 
+        //add time to the stopwatch
         value += Time.deltaTime;
+        //round to 1 decimal place
         double b = System.Math.Round(value, 1);
+        //set the text to the new string
         watch.text = b.ToString();
     }
 }
